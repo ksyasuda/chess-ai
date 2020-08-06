4e4fc3f0b7ed0c49221b2fb7fdc245f00d6ce954
ksyasuda <ksyasuda@umich.edu>
2020-08-05 22:51:08 -0700

    Add colors array

--- headers/board.h
+++ headers/board.h
@@ -4,9 +4,23 @@
 #include <vector>
 // #include <string>
 
+constexpr auto RED = "\033[31m";
+constexpr auto GREEN = "\033[32m";
+constexpr auto YELLOW = "\033[33m";
+constexpr auto BLUE = "\033[34m";
+constexpr auto WHITE = "\033[90m";
+constexpr auto ORANGE = "\033[96m";
+constexpr auto BLACK = "\033[93m";
+constexpr auto PURPLE = "\033[95m";
+constexpr auto CYAN = "\033[36m";
+constexpr auto RESET = "\033[0m";
+
+
+
 class Board {
 public:
 	std::vector<std::vector<char>> board;
+	const char* colors[11] = {RED, ORANGE, YELLOW, GREEN, BLUE, CYAN, PURPLE, BLACK, WHITE, RESET };
 
 	void initBoard();
 
